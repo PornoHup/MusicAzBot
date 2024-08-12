c1edba8579b035e5041654483c125d8145577e6e
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-08-13 00:53:41 +0400

    Update config.py

--- MusicAzBot/config.py
+++ MusicAzBot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-   API_ID = int(os.getenv("API_ID", "8953338"))
-   API_HASH = os.getenv("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "2142897671:ghyujjhgggggggggggggfddddddddddddtttttttttttttt")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "MusicAzBot")
-   OWNER_NAME = os.environ.get("OWNER_NAME", "HuseynH") 
-   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "MusicAzPlaylist")
-   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001692410500"))
-   ALIVE_NAME = os.environ.get("ALIVE_NAME", "HUSEYN")
+   API_ID = int(os.getenv("API_ID", "26170578"))
+   API_HASH = os.getenv("API_HASH", "4ce2d5627427004aed3e0f421e65f0e4")
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "7010221401:AAHegnqsvMW_mh0zCQU-TFAgq2qBEMcb0q8")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "nezrinsongbot")
+   OWNER_NAME = os.environ.get("OWNER_NAME", "thagiyev") 
+   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "nezrinlogo")
+   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002168356385"))
+   ALIVE_NAME = os.environ.get("ALIVE_NAME", "Nezrin")
    ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/dc9794139c12507f5eb1c.jpg")    
